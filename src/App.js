e7d8ed8c39c2be1d14f1f69a6069f11c13f20488
Lavruk Yulia <lavruk_yulia@ukr.net>
2021-02-20 18:05:49 +0200

    add redux, axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container"></div>
+    </div>
+  );
 }
 
 export default App;
